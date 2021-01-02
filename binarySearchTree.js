dfc44f73b7b868f3419ed8b63d3beec78a9f1504
ItaloPussi <italopussi@gmail.com>
2021-01-02 15:16:01 -0300

    Data Structure: Traversal and Height (Binary Search Tree)

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -73,6 +73,36 @@ class BST {
         return current
     }
 
+    isBalanced() {
+        return (this.findMinHeight() >= this.findMaxHeight() -1)
+    }
+
+    findMinHeight(node = this.root){
+        if (node == null){
+            return -1
+        }
+        let left = this.findMinHeight(node.left)
+        let right = this.findMinHeight(node.right)
+        if (left < right) {
+            return left + 1
+        } else {
+            return right + 1
+        }
+    }
+
+    findMaxHeight(node = this.root){
+        if (node == null) {
+            return -1
+        }
+        let left = this.findMaxHeight(node.left)
+        let right = this.findMaxHeight(node.right)
+
+        if(left > right) {
+            return left + 1
+        } else {
+            return right + 1
+        }
+    }
     isPresent(data){
         let current = this.root
         while (current){
@@ -89,56 +119,127 @@ class BST {
         }
         return false
     }
-
-     remove(data) {
-    const removeNode = function(node, data) {
-      if (node == null) {
-        return null;
-      }
-      if (data == node.data) {
-        // node has no children 
-        if (node.left == null && node.right == null) {
-          return null;
+    inOrder(){
+        if(this.root == null){
+            return null
+        }else {
+            let result = new Array()
+            function traverseInOrder(node){
+                node.left && traverseInOrder(node.left)
+                result.push(node.data)
+                node.right && traverseInOrder(node.right)
+            }
+            traverseInOrder(this.root)
+            return result
         }
-        // node has no left child 
-        if (node.left == null) {
-          return node.right;
-        }
-        // node has no right child 
-        if (node.right == null) {
-          return node.left;
-        }
-        // node has two children 
-        var tempNode = node.right;
-        while (tempNode.left !== null) {
-          tempNode = tempNode.left;
-        }
-        node.data = tempNode.data;
-        node.right = removeNode(node.right, tempNode.data);
-        return node;
-      } else if (data < node.data) {
-        node.left = removeNode(node.left, data);
-        return node;
-      } else {
-        node.right = removeNode(node.right, data);
-        return node;
-      }
     }
-    this.root = removeNode(this.root, data);
+    preOrder(){
+        if(this.root == null){
+            return null
+        }else {
+            let result = new Array()
+            function traversePreorder(node){
+                result.push(node.data)
+                node.left && traversePreorder(node.left)
+                node.right && traversePreorder(node.right)
+            }
+            traversePreorder(this.root)
+            return result
+        }
+    }
+
+    postOrder(){
+        if(this.root == null){
+            return null
+        }else {
+            let result = new Array()
+            function traverserPostOrder(node){
+                node.left && traverserPostOrder(node.left)
+                node.right && traverserPostOrder(node.right)
+                result.push(node.data)
+
+            }
+            traverserPostOrder(this.root)
+            return result
+        }
+    }
+    levelOrder() {
+        let result = []
+        let Q = [] 
+        if (this.root != null) {
+            Q.push(this.root)
+            while(Q.length > 0) {
+                let node = Q.shift()
+                result.push(node.data)
+                if (node.left != null) {
+                    Q.push(node.left)
+                }
+                if (node.right != null) {
+                    Q.push(node.right)
+                }
+            }
+            return result
+        } else {
+            return null
+        }
+    }
+    remove(data) {
+        const removeNode = function(node, data) {
+        if (node == null) {
+            return null
+        }
+        if (data == node.data) {
+            // node has no children 
+            if (node.left == null && node.right == null) {
+            return null
+            }
+            // node has no left child 
+            if (node.left == null) {
+            return node.right
+            }
+            // node has no right child 
+            if (node.right == null) {
+            return node.left
+            }
+            // node has two children 
+            var tempNode = node.right
+            while (tempNode.left !== null) {
+            tempNode = tempNode.left
+            }
+            node.data = tempNode.data
+            node.right = removeNode(node.right, tempNode.data)
+            return node
+        } else if (data < node.data) {
+            node.left = removeNode(node.left, data)
+            return node
+        } else {
+            node.right = removeNode(node.right, data)
+            return node
+        }
+    }
+    this.root = removeNode(this.root, data)
   }
 }
 
 const bst = new BST()
+bst.add(9)
 bst.add(4)
-bst.add(2)
-bst.add(6)
-bst.add(1)
+bst.add(17)
 bst.add(3)
+bst.add(6)
+bst.add(22)
 bst.add(5)
 bst.add(7)
-bst.remove(4)
-console.log(bst.findMin())
-console.log(bst.findMax())
-bst.remove(7)
-console.log(bst.findMax())
-console.log(bst.isPresent(4))
+bst.add(20)
+
+console.log(bst.findMinHeight())
+console.log(bst.findMaxHeight())
+console.log(bst.isBalanced())
+bst.add(10)
+console.log(bst.findMinHeight())
+console.log(bst.findMaxHeight())
+console.log(bst.isBalanced())
+console.log("In order: " + bst.inOrder())
+console.log("Pre order: " + bst.preOrder())
+console.log("Post order: " + bst.postOrder())
+console.log("Level order: " + bst.levelOrder())
